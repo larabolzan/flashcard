59a5edbc8ac6abe52030216533c6dcc3e7614bd3
larabolzan <160867540+larabolzan@users.noreply.github.com>
2024-11-05 08:10:57 -0300

    Add files via upload

---
+++ pergunstas.js
@@ -0,0 +1,17 @@
+CriaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'A capital é Brasília'
+)
+
+CriaCartao(
+    'Geograia',
+    'Qual é a capital da França?',
+    'A capital é Paris'
+)
+
+CriaCartao(
+    'Geografia',
+    'Qual a capital da Almanha?',
+    'A capial é Berlim'
+)
